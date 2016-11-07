9d0df71c53eb48c62c83115c95443a62308defb3
Ooky <blki@hotmail.com>
2016-11-07 15:34:44 +0100

    Init Commit

---
+++ desktop/src/ch/creatif/swipeup/editor/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ch.creatif.swipeup.editor.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ch.creatif.swipeup.editor.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
