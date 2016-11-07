2481f9e408d1fddd0c1a7551bcc001b1301ed35d
Ooky <blki@hotmail.com>
2016-11-07 17:22:31 +0100

    Added Icon + first Screen

---
+++ core/src/ch/creatif/swipeup/editor/Constants.java
@@ -0,0 +1,17 @@
+package ch.creatif.swipeup.editor;
+
+/**
+ *
+ * @author Creat-if
+ */
+public class Constants {
+
+//==============================================================================
+//Desktoplauncher
+//==============================================================================
+	public static int WINDOW_WIDTH = 1024;
+	public static int WINDOW_HEIGTH = 720;
+	public static String TITLE = "SwipeUp! - Editor";
+	public static final String FAVICON = "Graphics/Icon/Icon.png";
+
+}
--- desktop/src/ch/creatif/swipeup/editor/desktop/DesktopLauncher.java
+++ desktop/src/ch/creatif/swipeup/editor/desktop/DesktopLauncher.java
@@ -1,12 +1,27 @@
 package ch.creatif.swipeup.editor.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import ch.creatif.swipeup.editor.Constants;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import ch.creatif.swipeup.editor.Main;
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 
+/**
+ *
+ * @author Creat-if
+ */
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+	public static void main(String[] arg) {
+		LwjglApplicationConfiguration config;
+		config = new LwjglApplicationConfiguration();
+
+		config.width = Constants.WINDOW_WIDTH;
+		config.height = Constants.WINDOW_HEIGTH;
+		config.title = Constants.TITLE;
+		config.resizable = false;
+		config.addIcon(Constants.FAVICON, Files.FileType.Internal);
 		new LwjglApplication(new Main(), config);
+
 	}
 }
